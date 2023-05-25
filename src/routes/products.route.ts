dd3b8a4422393e0ca9bc57cc61429a22852376d3
calebeLAR <ca6ele@gmail.com>
2023-05-25 17:38:46 -0400

    req02: cria rota GET /products

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -4,5 +4,6 @@ import productController from '../controllers/product.controller';
 const productsRoute = express.Router();
 
 productsRoute.post('/products', productController.create);
+productsRoute.get('/products', productController.getAll);
 
 export default productsRoute;
